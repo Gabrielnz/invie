9720e5b59ff778999967d48463d76329e46a46e2
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-05-24 15:50:36 -0400

    Anidando clases de CSS con Nested

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var postcss = require('gulp-postcss')
 var autoprefixer = require('autoprefixer')
+var cssnested = require('postcss-nested')
 var browserSync = require('browser-sync').create()
 
 // Servidor de desarrollo
@@ -19,7 +20,8 @@ gulp.task('css', function () {
   // Utiliza los autoprefixers de los navegadores con mas de 5% de uso
   // y de internet explorer 8 en adelante
   var processors = [
-    autoprefixer({ browsers: ['> 5%', 'ie 8'] })
+    autoprefixer({ browsers: ['> 5%', 'ie 8'] }),
+    cssnested
   ]
 
   return gulp.src('./src/*.css')
